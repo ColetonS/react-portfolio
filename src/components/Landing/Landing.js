0a2a7491d1b5369b151f3199a0b9129aa78d4e70
ColetonS <coleton.m.smith@gmail.com>
2019-10-03 11:58:33 -0600

    added more stars

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -7,10 +7,15 @@ export default function Landing() {
             <h1>Coleton Smith</h1>
             <h2>Web Developer</h2>
             <div className="star-container">
-                <div className="star"></div>
+                <div className="star star-1"></div>
                 <div className="star star-2"></div>
                 <div className="star star-3"></div>
                 <div className="star star-4"></div>
+                <div className="star star-5"></div>
+                <div className="star star-6"></div>
+            </div>
+            <div className="star-container-2">
+                <div className="star star-7"></div>
             </div>
         </div>
     )
