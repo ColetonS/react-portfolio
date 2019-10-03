3c6b521a92f9c40aefd19bb3f98b6fee12c5d69a
ColetonS <coleton.m.smith@gmail.com>
2019-10-03 11:08:24 -0600

    initial shooting stars

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -4,7 +4,14 @@ import './Landing.scss'
 export default function Landing() {
     return (
         <div className='landing-container'>
-            <h1>Welcome</h1>
+            <h1>Coleton Smith</h1>
+            <h2>Web Developer</h2>
+            <div className="star-container">
+                <div className="star"></div>
+                <div className="star star-2"></div>
+                <div className="star star-3"></div>
+                <div className="star star-4"></div>
+            </div>
         </div>
     )
 }
