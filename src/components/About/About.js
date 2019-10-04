a908bc2771cd47e51715bb9a4ce5a1b4026591b2
ColetonS <coleton.m.smith@gmail.com>
2019-10-04 12:06:56 -0600

    set up About page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import './About.scss'
+import React from "react";
+import "./About.scss";
 
 export default function About() {
-    return (
-        <div className='about-container'>
-            About Me
-        </div>
-    )
+  return (
+    <div className="about-container">
+      <div className='about-description'>
+        <h1>About Me</h1>
+        <h4>
+          I'm a full-stack web developer with an emphasis on React, Node,
+          Express, and PostgreSQL. <br /> I also write short stories, ski Utah's
+          canyons, and try to befriend any dog I encounter.
+        </h4>
+      </div>
+    </div>
+  );
 }
