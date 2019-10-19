0ad1fa9ec5653de4e858b926b6cec3d332d7157a
ColetonS <coleton.m.smith@gmail.com>
2019-10-19 10:49:37 -0600

    added project images to Projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import './Projects.scss'
+import writeon from '../../images/write-on.png'
+import youteachme from '../../images/youteachme.png'
 
 export default function Projects() {
     return (
         <div className='projects-container'>
-            Projects
+            <h1>Projects</h1>
+            <div className="projects">
+                <div className="project">
+                    <h2>Write On</h2>
+                    <img className='project-img' alt='app image' src={writeon} />
+                </div>
+                <div className="project">
+                    <h2>youTeachMe</h2>
+                    <img className='project-img' alt='app image' src={youteachme} />
+                </div>
+            </div>
         </div>
     )
 }
